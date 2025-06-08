88be02b533e7ff42f4ab086f5e15d284558099ec
Zlakesyx <jdcoffey54@gmail.com>
2025-06-07 19:10:47 -0700

    added block type identification

--- src/block/block_converter.py
+++ src/block/block_converter.py
@@ -1,3 +1,17 @@
+from enum import Enum
+
+import re
+
+
+class BlockType(str, Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDERED_LIST = "unordered_list"
+    ORDERED_LIST = "ordered_list"
+
+
 def markdown_to_blocks(text: str) -> list[str]:
     """
     # This is a heading
@@ -10,3 +24,40 @@ def markdown_to_blocks(text: str) -> list[str]:
     """
     blocks = text.strip().split("\n\n")
     return blocks
+
+
+def block_to_block_type(block: str) -> BlockType:
+    if re.match(r"^(#{1,6})\s+(.*)", block):
+        return BlockType.HEADING
+    elif re.match(r"^(`{3})(.*)(`{3})$", block):
+        return BlockType.CODE
+    elif _lines_match(block, ">"):
+        return BlockType.QUOTE
+    elif _lines_match(block, "- "):
+        return BlockType.UNORDERED_LIST
+    elif _is_ordered_list(block):
+        return BlockType.ORDERED_LIST
+    else:
+        return BlockType.PARAGRAPH
+
+
+def _lines_match(block: str, delimeter: str) -> bool:
+    lines = block.split("\n")
+    for line in lines:
+        match = re.match(rf"^{delimeter}.*", block)
+        if not match:
+            return False
+    return True
+
+
+def _is_ordered_list(block: str) -> bool:
+    lines = block.split("\n")
+    prev = 0
+    for line in lines:
+        current = re.findall(r"^(\d+)\.\s.*", line)
+        if not current:
+            return False
+        if int(current[0]) - 1 != prev:
+            return False
+        prev += 1
+    return True
--- src/block/test_block_converter.py
+++ src/block/test_block_converter.py
@@ -1,6 +1,6 @@
 import unittest
 
-from block.block_converter import markdown_to_blocks
+from block.block_converter import block_to_block_type, markdown_to_blocks, BlockType
 
 md = """
 This is **bolded** paragraph
@@ -12,19 +12,94 @@ This is the same paragraph on a new line
 - with items
 """
 
+blocks = [
+    "This is **bolded** paragraph",
+    "This is another paragraph with _italic_ text and `code` here\nThis is the same paragraph on a new line",
+    "- This is a list\n- with items",
+]
+
 
 class TestBlockConverter(unittest.TestCase):
-        def test_markdown_to_blocks(self):
-            blocks = markdown_to_blocks(md)
-            print(blocks)
-            self.assertEqual(
-                blocks,
-                [
-                    "This is **bolded** paragraph",
-                    "This is another paragraph with _italic_ text and `code` here\nThis is the same paragraph on a new line",
-                    "- This is a list\n- with items",
-                ],
-            )
+    def test_markdown_to_blocks(self):
+        self.assertEqual(markdown_to_blocks(md), blocks)
+
+        # return BlockType.UNORDERED_LIST
+        # return BlockType.ORDERED_LIST
+
+    def test_block_to_heading(self):
+        inputs = [
+            "# heading",
+            "## heading",
+            "### heading",
+            "#### heading",
+            "##### heading",
+            "###### heading",
+        ]
+        # too many #
+        not_heading = "####### not heading"
+        for input in inputs:
+            self.assertEqual(block_to_block_type(input), BlockType.HEADING)
+        self.assertNotEqual(block_to_block_type(not_heading), BlockType.HEADING)
+
+    def test_block_to_code(self):
+        inputs = [
+            "``` this is a code block ```",
+            "```this is a code block```",
+        ]
+        not_code = [
+            "not code```this is not a code block```",
+            "```this is not a code block``` not code",
+        ]
+        for input in inputs:
+            self.assertEqual(block_to_block_type(input), BlockType.CODE)
+        for input in not_code:
+            self.assertNotEqual(block_to_block_type(input), BlockType.CODE)
+
+    def test_block_to_quote(self):
+        inputs = [
+            "> quote",
+            "> quote\n>quote",
+        ]
+        not_quote = [
+            "!> quote",
+            "\n> quote\n>quote",
+        ]
+        for input in inputs:
+            self.assertEqual(block_to_block_type(input), BlockType.QUOTE)
+        for input in not_quote:
+            self.assertNotEqual(block_to_block_type(input), BlockType.QUOTE)
+
+    def test_block_to_unordered_list(self):
+        inputs = [
+            "- item",
+            "- item\n- item",
+        ]
+        not_unorderd_list = [
+            "!- item",
+            "\n- item\n-item",
+        ]
+        for input in inputs:
+            self.assertEqual(block_to_block_type(input), BlockType.UNORDERED_LIST)
+        for input in not_unorderd_list:
+            self.assertNotEqual(block_to_block_type(input), BlockType.UNORDERED_LIST)
+
+    def test_block_to_ordered_list(self):
+        inputs = [
+            "1. item",
+            "1. item\n2. item",
+        ]
+        not_unorderd_list = [
+            "2. item",
+            "-1. item\n2. item",
+        ]
+        for input in inputs:
+            self.assertEqual(block_to_block_type(input), BlockType.ORDERED_LIST)
+        for input in not_unorderd_list:
+            self.assertNotEqual(block_to_block_type(input), BlockType.ORDERED_LIST)
+
+    def test_block_to_paragraph(self):
+        paragraph = blocks[1]
+        self.assertEqual(block_to_block_type(paragraph), BlockType.PARAGRAPH)
 
 
 if __name__ == "__main__":
