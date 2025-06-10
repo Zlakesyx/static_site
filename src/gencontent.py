a47df6e21792cc333512742847e762f1cde82278
Zlakesyx <jdcoffey54@gmail.com>
2025-06-09 21:56:45 -0700

    recursive page generation

--- src/gencontent.py
+++ src/gencontent.py
@@ -1,5 +1,25 @@
 import os
+import shutil
 from markdown_blocks import markdown_to_html_node
+
+
+def generate_pages_recursive(dir_path_content: str, template_path: str, dest_dir_path: str):
+    if not os.path.exists(dest_dir_path):
+        os.mkdir(dest_dir_path)
+
+    for filename in os.listdir(dir_path_content):
+        from_path = os.path.join(dir_path_content, filename)
+        if os.path.isfile(from_path):
+            filename, extension = os.path.splitext(filename)
+            # Only generate from markdown files
+            if extension == ".md":
+                dest_path = os.path.join(dest_dir_path, filename + ".html")
+                generate_page(from_path, template_path, dest_path)
+        elif os.path.isdir(from_path):
+            dest_path = os.path.join(dest_dir_path, filename)
+            generate_pages_recursive(from_path, template_path, dest_path)
+        else:
+            continue
 
 
 def generate_page(from_path: str, template_path: str, dest_path: str):
